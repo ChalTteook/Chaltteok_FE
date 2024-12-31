e563951f65da7f4a8bdf9828290f5f72e0d6c790
전형준 <bingo502@naver.com>
2024-12-31 19:32:05 +0900

    initial commit

---
+++ src/main/java/com/chaltteok/chaltteok/ChaltteokApplication.java
@@ -0,0 +1,13 @@
+package com.chaltteok.chaltteok;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChaltteokApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChaltteokApplication.class, args);
+	}
+
+}
